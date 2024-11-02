46c3ebe66e2f56cc33c2216641411a9d99cf2bf5
fuuuu <sanchan.fuu@gmail.com>
2024-11-02 22:25:34 +0900

    feat: ログイン機能の仮実装

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
+import { Authenticator } from '@aws-amplify/ui-react';
 import outputs from "../amplify_outputs.json";
+import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Authenticator 
+      socialProviders={['google']} 
+      variation="modal"
+      hideSignUp={true}
+    >
+      <App />
+    </Authenticator>
   </React.StrictMode>
 );
